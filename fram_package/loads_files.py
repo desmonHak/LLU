55d7ef4d4607714f37de9abc6f95fb18155ab0a1
pan con queso <157338617+zetgit-dev@users.noreply.github.com>
2024-03-29 01:43:34 -0500

    fix(nada): void

--- fram_package/loads_files.py
+++ fram_package/loads_files.py
@@ -4,14 +4,14 @@ from random   import randint
 from requests import request, ConnectionError
 from colorama import Fore
 from json     import dumps
+from ast      import literal_eval
 
 from pygments            import highlight
 from pygments.formatters import Terminal256Formatter
 from pygments.lexers     import get_lexer_by_name
 
 from fram_package.get_info_system import ThisSysten
-from fram_package.error           import ConnectionError, NotFoundThisFile
-from fram_package.load_conf_user  import literal_eval
+from fram_package.error           import NotFoundThisFile
 
 excluir_directorios = [
     # directorios a excluir de la recopilacion
@@ -33,16 +33,16 @@ def load_file(file):
     Args:
         file (str): Nombre del archivo .json
     """
-    
+
     try:
         file = open(file, "r")
         # convertimos el json en un ficionario
         _file = literal_eval(file.read())
-        # cerrar el archivo 
+        # cerrar el archivo
         file.close()
-    
+
         return _file
-    except FileNotFoundError: 
+    except FileNotFoundError:
         print("Archivo de configuracion no encontrado: {}".format(file))
         raise NotFoundThisFile(file)
 
@@ -162,88 +162,87 @@ def write_dict_hash_dir(dict_hash_dir, file_name="file.json"):
     _file.write(str(dict_hash_dir))
     _file.close()
 
-def check_updates(users=["desmonHak"], url="https://raw.githubusercontent.com/{}/LLU/main/file.json", filename="file_check_update.json", debug=False, fileCheck="file.json"):
-    
-    _file = None
-    for user in users:
-        
-        try:
-            useragents = [
-                "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)",
-                "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)",
-                "Googlebot/2.1 (http://www.googlebot.com/bot.html)",
-                "Opera/9.20 (Windows NT 6.0; U; en)",
-                "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.1) Gecko/20061205 Iceweasel/2.0.0.1 (Debian-2.0.0.1+dfsg-2)",
-                "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; FDM; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 1.1.4322)",
-                "Opera/10.00 (X11; Linux i686; U; en) Presto/2.2.0",
-                "Mozilla/5.0 (Windows; U; Windows NT 6.0; he-IL) AppleWebKit/528.16 (KHTML, like Gecko) Version/4.0 Safari/528.16",
-                "Mozilla/5.0 (compatible; Yahoo! Slurp/3.0; http://help.yahoo.com/help/us/ysearch/slurp)", 
-                "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.13) Gecko/20101209 Firefox/3.6.13"
-                "Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 5.1; Trident/5.0)",
-                "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
-                "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 6.0)",
-                "Mozilla/4.0 (compatible; MSIE 6.0b; Windows 98)",
-                "Mozilla/5.0 (Windows; U; Windows NT 6.1; ru; rv:1.9.2.3) Gecko/20100401 Firefox/4.0 (.NET CLR 3.5.30729)",
-                "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.8) Gecko/20100804 Gentoo Firefox/3.6.8",
-                "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.7) Gecko/20100809 Fedora/3.6.7-1.fc14 Firefox/3.6.7",
-                "Mozilla/5.0 (compatible; Googlebot/2.1; http://www.google.com/bot.html)",
-                "Mozilla/5.0 (compatible; Yahoo! Slurp; http://help.yahoo.com/help/us/ysearch/slurp)",
-                "YahooSeeker/1.2 (compatible; Mozilla 4.0; MSIE 5.5; yahooseeker at yahoo-inc dot com ; http://help.yahoo.com/help/us/shop/merchant/)"
-            ]
-            
-            headers = {
-                "User-Agent": useragents[randint(0, len(useragents)-1)],
-                "accept": "*/*",               
-                "accept-encoding": "gzip;deflate;br" 
-            }
-            response = request("GET", url.format(user), headers = headers)
-            
-            _file = open(filename, "w")
-            _file.write(response.text)
-            _file.close()
-            
-            _file = response.text
-            
-            """carpetaDescargas = "{}{}{}".format(getcwd(), splas, tempDir)
-            print(carpetaDescargas)
-            archivo = ZipFile(BytesIO(response.content))
-            archivo.extractall(carpetaDescargas)        
-            print(dir(archivo)    )
-            if debug: print(archivo.printdir())
-            
-            
-            # obtenemos el nombre de la carpeta donde esta todos los archivos descargados:
-            file_name = archivo.infolist()[0].filename"""
-            
-            dataDownload = load_file(filename)
-            dataOriginal = load_file(fileCheck)
-            
-            if len(dataDownload) != len(dataOriginal):
-                
-                print(f'Datos nuevos: {highlight(formater_to_json(dataDownload), lexer= get_lexer_by_name("json"), formatter=Terminal256Formatter(style="dracula"))}\nDatos antiguos: {highlight(formater_to_json(dataOriginal), lexer= get_lexer_by_name("json"), formatter=Terminal256Formatter(style="dracula"))}\nA habido cambios en el tamano de los archivos de hash\'h, por lo que hay actualizacion.')
-                # son diferentes, retornar True, hay actualizacion
-                return True
-            else:
-                for _hash in dataOriginal.keys():
-                    if _hash in dataDownload:
-                        print(f"El hash ({Fore.LIGHTMAGENTA_EX}{dataDownload[_hash]}{Fore.RESET}) del archivo ({Fore.LIGHTGREEN_EX}{_hash}{Fore.RESET}) es correcto")
-                    else:
-                        print("Al parecer hubo cambios de esta version ({})".format(_hash))
-                        return True # hay hash's diferentes, actualizacion
-                return False # si no se detecto hash's diferenetes y el diccionario es el mismo, no hay actualizacion
-            
-            break
-        except ConnectionError:
-            print("No se pudo obtener los datos desde ({})".format(url.format(user)))
-        
-    if _file != None: return _file
-    else: raise ConnectionError
+def check_updates(user="desmonHak", url="https://raw.githubusercontent.com/{}/LLU/main/file.json", filename="file_check_update.json", debug=False, fileCheck="file.json"):
+
+    file = None
+    useragents = [
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)",
+        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)",
+        "Googlebot/2.1 (http://www.googlebot.com/bot.html)",
+        "Opera/9.20 (Windows NT 6.0; U; en)",
+        "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.1) Gecko/20061205 Iceweasel/2.0.0.1 (Debian-2.0.0.1+dfsg-2)",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; FDM; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 1.1.4322)",
+        "Opera/10.00 (X11; Linux i686; U; en) Presto/2.2.0",
+        "Mozilla/5.0 (Windows; U; Windows NT 6.0; he-IL) AppleWebKit/528.16 (KHTML, like Gecko) Version/4.0 Safari/528.16",
+        "Mozilla/5.0 (compatible; Yahoo! Slurp/3.0; http://help.yahoo.com/help/us/ysearch/slurp)",
+        "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.13) Gecko/20101209 Firefox/3.6.13"
+        "Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 5.1; Trident/5.0)",
+        "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+        "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 6.0)",
+        "Mozilla/4.0 (compatible; MSIE 6.0b; Windows 98)",
+        "Mozilla/5.0 (Windows; U; Windows NT 6.1; ru; rv:1.9.2.3) Gecko/20100401 Firefox/4.0 (.NET CLR 3.5.30729)",
+        "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.8) Gecko/20100804 Gentoo Firefox/3.6.8",
+        "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.7) Gecko/20100809 Fedora/3.6.7-1.fc14 Firefox/3.6.7",
+        "Mozilla/5.0 (compatible; Googlebot/2.1; http://www.google.com/bot.html)",
+        "Mozilla/5.0 (compatible; Yahoo! Slurp; http://help.yahoo.com/help/us/ysearch/slurp)",
+        "YahooSeeker/1.2 (compatible; Mozilla 4.0; MSIE 5.5; yahooseeker at yahoo-inc dot com ; http://help.yahoo.com/help/us/shop/merchant/)"
+    ]
+
+    headers = {
+        "User-Agent": useragents[randint(0, len(useragents)-1)],
+        "accept": "*/*",
+        "accept-encoding": "gzip;deflate;br"
+    }
+
+    try:
+        response = request("GET", url.format(user), headers = headers)
+
+        with open(filename, 'w') as file:
+            file.write(response.text)
+
+        file = response.text
+
+        """carpetaDescargas = "{}{}{}".format(getcwd(), splas, tempDir)
+        print(carpetaDescargas)
+        archivo = ZipFile(BytesIO(response.content))
+        archivo.extractall(carpetaDescargas)
+        print(dir(archivo)    )
+        if debug: print(archivo.printdir())
+
+
+        # obtenemos el nombre de la carpeta donde esta todos los archivos descargados:
+        file_name = archivo.infolist()[0].filename"""
+
+        dataDownload = load_file(filename)
+        dataOriginal = load_file(fileCheck)
+
+        if len(dataDownload) != len(dataOriginal):
+
+            print(f'Datos nuevos: {highlight(formater_to_json(dataDownload), lexer= get_lexer_by_name("json"), formatter=Terminal256Formatter(style="dracula"))}\nDatos antiguos: {highlight(formater_to_json(dataOriginal), lexer= get_lexer_by_name("json"), formatter=Terminal256Formatter(style="dracula"))}\nA habido cambios en el tamano de los archivos de hash\'h, por lo que hay actualizacion.')
+            # son diferentes, retornar True, hay actualizacion
+            return True
+        else:
+            for _hash in dataOriginal.keys():
+                if _hash in dataDownload:
+                    print(f"El hash ({Fore.LIGHTMAGENTA_EX}{dataDownload[_hash]}{Fore.RESET}) del archivo ({Fore.LIGHTGREEN_EX}{_hash}{Fore.RESET}) es correcto")
+                else:
+                    print("Al parecer hubo cambios de esta version ({})".format(_hash))
+                    return True # hay hash's diferentes, actualizacion
+            return False # si no se detecto hash's diferenetes y el diccionario es el mismo, no hay actualizacion
+
+    except ConnectionError:
+        print("No se pudo obtener los datos desde ({})".format(url.format(user)))
+
+    if file is None:
+        return file
+    else:
+        raise ConnectionError
 
 if __name__ == "__main__":
+    print(check_updates())
+    # tree_dir = get_directory(debug=False)
+    # print_tree(tree_dir, excluir_files=excluir_archivos)
 
-    tree_dir = get_directory(debug=False)
-    print_tree(tree_dir, excluir_files=excluir_archivos)
-
-    dict_hash_dir = get_hash(tree_dir)
-    print_dict_hash_dir(dict_hash_dir)
-    write_dict_hash_dir(dict_hash_dir)
+    # dict_hash_dir = get_hash(tree_dir)
+    # print_dict_hash_dir(dict_hash_dir)
+    # write_dict_hash_dir(dict_hash_dir)
--- llu.py
+++ llu.py
@@ -2,7 +2,7 @@ from sys import argv
 
 from fram_package.main           import main
 from fram_package.load_conf_user import literal_eval
-from fram_package.loads_files    import *
+from fram_package.loads_files    import get_directory, print_tree, get_hash, write_dict_hash_dir, check_updates
 
 with open("information.json", "r") as file:
     information = literal_eval(file.read())
@@ -21,8 +21,7 @@ if __name__ == "__main__":
         dict_hash_dir = get_hash(tree_dir)
         write_dict_hash_dir(dict_hash_dir)
 
-    if check_updates(): print("Hay una nueva version de este software!!!")
+    if check_updates():
+        print("Hay una nueva version de este software!!!")
 
     main()
-
-    
